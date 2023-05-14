ab79eb3f095329dbc6cd848530452364eab8c3dd
HuyNChu <chuhuyrpi@gmail.com>
2023-05-14 13:56:17 +0700

    http server boilerplate complete

--- main.go
+++ main.go
@@ -28,9 +28,14 @@ func main() {
 		ExposedHeaders:		[]string{"Link"},
 		AllowCredentials:	false,
 		MaxAge:		 				300,
-
-
 	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+
+	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
